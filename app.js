1e4dabea63741644173e91cdfc622a873019e039
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2020-11-23 09:11:19 +0530

    first debug inthe link id extractionnnno

--- app.js
+++ app.js
@@ -12,23 +12,17 @@ app.get('/', (req, res)=>{
     res.send('Hello World!')
 })
 
-app.get('/getytvideo/:id', (req, res)=>{
-    // let info = await ytdl.getInfo(req.params.id);
-    // let format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-    // console.log('Format found!', format.url);
-    // res.json({
-    //     downloadlink: format.url
-    // })
-    // console.log(req.params.id);
-    const url = `https://www.youtube.com/watch?v=${req.params.id}`
+app.get('/getytvideo/:id', async (req, res)=>{
+
+    const url = await `https://www.youtube.com/watch?v=${req.params.id}`
     youtubedl.getInfo(url, (err, info) => {
         if (err) {console.log(err)}
         res.json({
-            Videolink: info.url || 'https://r2---sn-gwpa-ccpd.googlevideo.com/videoplayback?expire=1606123277&ei=rSq7X8mtJYiUvQS1toHYBg&ip=2409%3A4062%3A2e8e%3Afc51%3A3839%3A2333%3Aac50%3Aa6c8&id=o-AEivPWotY0c59qvVCsmY6SVQSJ_BjOaSLZLfmHcFPMoZ&itag=22&source=youtube&requiressl=yes&mh=Ut&mm=31%2C29&mn=sn-gwpa-ccpd%2Csn-cvh76nek&ms=au%2Crdu&mv=m&mvi=2&pcm2cms=yes&pl=41&gcr=in&initcwndbps=196250&vprv=1&mime=video%2Fmp4&ns=BDSPbzJTzwRAHsJ0iTqu878F&ratebypass=yes&dur=48.065&lmt=1605973615624921&mt=1606101391&fvip=2&beids=9466588&c=WEB&txp=5532434&n=QaQBmA59saq5dEL&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cns%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRAIgXTybaR_fl73fISG3QEIvWLznjNNmtL5PEYRmJHfu3ngCICBORQrOIGB-SmLgd1XxhSMCAbLfJLp9AX6L2PAu5z-i&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIgLwlxOzLHi-dsFiiQzmkicNFw8WGeXSZdvRUBDoMMdTkCIQCJzj4BM0gydLQ9lUaVP8oNh-Fwb3BgqTtmxbW19rJ5_Q%3D%3D',
-            title: info.title || 'The Title Track Is As Groovy As The Whole Series | SCAM 1992 - The Harshad Mehta Story',
-            formate: info.format_id || '22',
-            thumbnail: info.thumbnail || 'https://i.ytimg.com/vi/p3OUFMpT7B0/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDrn5n946PhgsZAZTdz6df0gq-NPQ',
-            description: info.description || 'fsdhgsdhdgc rh sdfh dh zdedyuejryi rkrj drjerje',
+            Videolink: info.url,
+            title: info.title,
+            formate: info.format_id,
+            thumbnail: info.thumbnail,
+            description: info.description
         })
         // console.log(info);
     })
